136cabd512f136d856dfc8735cd68a64e2629909
arthurvaisse-lesteven <arthurvaisse@yahoo.fr>
2026-01-20 20:42:09 +0100

    display the ranking as a table

--- classement_master/2026/ui-components.js
+++ classement_master/2026/ui-components.js
@@ -10,6 +10,13 @@ class VisualisationClassementQualificatif extends HTMLElement {
 
     render() {
         this.innerHTML += `
+            <div class="header-line">
+                <span class="col-rank">Rang</span>
+                <span class="col-username">Pseudo</span>
+                <span class="col-events">Evenements</span>
+                <span class="col-points">Points</span>
+                <span class="col-region">Région</span>
+            </div>
             <div class="players">
                 ${this.classementQualificatif.classement().map((joueur, index) => {
                     return `<classement-individuel
@@ -36,14 +43,11 @@ class ClassementIndividuel extends HTMLElement {
     render() {
         this.innerHTML = `
             <div class="player-card">
-                <div class="player-rank">#${this.getAttribute('rang')}</div>
-                <div class="player-info">
-                    <div class="player-username">${this.getAttribute('pseudonyme')}</div>
-                    <div class="player-stats">
-                        <span class="player-events">Events: ${this.getAttribute('evenements')}</span>
-                        <span class="player-points">Points: ${this.getAttribute('points')}</span>
-                    </div>
-                </div>
+                <span class="col-rank">#${this.getAttribute('rang')}</span>
+                <span class="col-username">${this.getAttribute('pseudonyme')}</span>
+                <span class="col-events">Events: ${this.getAttribute('evenements')}</span>
+                <span class="col-points">Points: ${this.getAttribute('points')}</span>
+                <span class="col-region">Région: TODO</span>
             </div>
         `;
     }
