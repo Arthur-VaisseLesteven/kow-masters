fe5a6b8f8eab3ebe3c37938b292d76706a5c153c
Bruno <br.gavazzi@gmail.com>
2026-06-30 13:08:28 +0200

    correction de ceil en floor pour le calcul des points par event

--- classement_master/2026/EvenementQualificatif.js
+++ classement_master/2026/EvenementQualificatif.js
@@ -3,92 +3,101 @@
  * Un événement possede un nom, une durée (journée ou week-end) et un classement des participants.
  */
 class EvenementQualificatif {
-    constructor(donnees) {
-        this.donnees = donnees;
+  constructor(donnees) {
+    this.donnees = donnees;
+  }
+
+  resultats() {
+    let resultatsIndividuels = [];
+
+    for (let i = 0; i < this.donnees.classement.length; i++) {
+      resultatsIndividuels.push({
+        pseudonyme: this.donnees.classement[i],
+        classementIndividuel: {
+          nom: this.donnees.nom,
+          score: this.score(
+            i,
+            this.donnees.classement.length,
+            this.donnees.duree,
+          ),
+          rang: i + 1,
+          totalParticipants: this.donnees.classement.length,
+        },
+      });
     }
 
-    resultats() {
-        let resultatsIndividuels = [];
+    return resultatsIndividuels;
+  }
 
-        for (let i = 0; i < this.donnees.classement.length; i++) {
-            resultatsIndividuels.push({
-                pseudonyme: this.donnees.classement[i],
-                classementIndividuel: {
-                    nom: this.donnees.nom,
-                    score: this.score(i, this.donnees.classement.length, this.donnees.duree),
-                    rang: i+1,
-                    totalParticipants: this.donnees.classement.length
-                }
-            })
-        }
+  score(classement, nombreDeParticipants, duree) {
+    return Math.max(
+      0,
+      Math.floor(nombreDeParticipants / 2) -
+        classement +
+        this.bonusPoints(classement, duree),
+    );
+  }
 
-        return resultatsIndividuels;
-    }
+  bonusPoints(classement, duree) {
+    if (duree === "journée") return 0;
+    if (classement === 0) return 6;
+    if (classement === 1) return 3;
+    if (classement === 2) return 1;
+    else return 0;
+  }
 
-    score(classement, nombreDeParticipants, duree) {
-        return Math.max(0, Math.ceil(nombreDeParticipants / 2) - (classement) + this.bonusPoints(classement, duree));
-    }
-
-    bonusPoints(classement, duree) {
-        if (duree === 'journée') return 0
-        if (classement === 0) return 6
-        if (classement === 1) return 3
-        if (classement === 2) return 1
-        else return 0;
-    }
-
-    static nommé(nom) {
-        return new EvenementQualificatifBuilder(nom);
-    }
+  static nommé(nom) {
+    return new EvenementQualificatifBuilder(nom);
+  }
 }
 
 class EvenementQualificatifBuilder {
-    constructor(nom) {
-        this.nom = nom;
-        this.duree = null;
-        this.classement = [];
-    }
+  constructor(nom) {
+    this.nom = nom;
+    this.duree = null;
+    this.classement = [];
+  }
 
-    surUnJour() {
-        this.duree = 'journée';
-        return this;
-    }
+  surUnJour() {
+    this.duree = "journée";
+    return this;
+  }
 
-    surUnWeekEnd() {
-        this.duree = 'week-end';
-        return this;
-    }
+  surUnWeekEnd() {
+    this.duree = "week-end";
+    return this;
+  }
 
-    avecPourClassement() {
-        return new ClassementBuilder(this);
-    }
+  avecPourClassement() {
+    return new ClassementBuilder(this);
+  }
 
-    construire() {
-        return new EvenementQualificatif({
-            nom: this.nom,
-            duree: this.duree,
-            classement: this.classement
-        });
-    }
+  construire() {
+    return new EvenementQualificatif({
+      nom: this.nom,
+      duree: this.duree,
+      classement: this.classement,
+    });
+  }
 }
 
 class ClassementBuilder {
-    constructor(evenementBuilder) {
-        this.evenementBuilder = evenementBuilder;
-    }
+  constructor(evenementBuilder) {
+    this.evenementBuilder = evenementBuilder;
+  }
 
-    enPremier(joueur) {
-        this.evenementBuilder.classement.push(joueur);
-        return this;
-    }
+  enPremier(joueur) {
+    this.evenementBuilder.classement.push(joueur);
+    return this;
+  }
 
-    puis(joueur) {
-        this.evenementBuilder.classement.push(joueur);
-        return this;
-    }
+  puis(joueur) {
+    this.evenementBuilder.classement.push(joueur);
+    return this;
+  }
 
-    enDernier(joueur) {
-        this.evenementBuilder.classement.push(joueur);
-        return this.evenementBuilder.construire();
-    }
+  enDernier(joueur) {
+    this.evenementBuilder.classement.push(joueur);
+    return this.evenementBuilder.construire();
+  }
 }
